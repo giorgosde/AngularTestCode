7ff208f61b0ba5b7127deaf71d3ddcef8ab034f4
George <gdeliandreadis@gmail.com>
2017-06-24 21:40:27 +0100

    #1:Create project structure

---
+++ src/app/ngComponents/app-component/AppComponent.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-component',
+  templateUrl: './templates/app-component.html',
+  styleUrls: ['./css/style.css'],
+})
+export class AppComponent  { name = 'Angular'; }
---
+++ src/app/ngServices/ListService.ts
@@ -0,0 +1,6 @@
+import { Injectable } from '@angular/core';
+//behavioSubject
+
+@Injectable()
+export class ListService {
+}
