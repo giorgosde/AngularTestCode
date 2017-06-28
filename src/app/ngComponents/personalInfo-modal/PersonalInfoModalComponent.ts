3b159637b50aa573e4ced401ec3761ff60961e00
George <gdeliandreadis@gmail.com>
2017-06-28 18:14:59 +0100

    #6: Created custom modal

--- src/app/ngComponents/personalInfo-modal/PersonalInfoModalComponent.ts
+++ src/app/ngComponents/personalInfo-modal/PersonalInfoModalComponent.ts
@@ -3,9 +3,19 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'personalInfo-modal',
   templateUrl: './templates/personalInfo-modal.html',
-  styleUrls: ['./css/style.css'],
+  styleUrls: ['./css/style.css']
 })
 export class PersonalInfoModalComponent  {
   @Input() user:any;
+  displayValue:any="none";
   constructor(){};
+   
+showModal() {
+    this.displayValue="block";
+}
+
+closeModal() {
+    this.displayValue="none";
+}
+
 }
