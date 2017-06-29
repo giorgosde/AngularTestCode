66bcef6ec705215fc9ad2371c0e8387cf509766e
George <gdeliandreadis@gmail.com>
2017-06-29 13:09:28 +0100

    #7: routing setup complete

--- src/app/ngModules/AppModule.ts
+++ src/app/ngModules/AppModule.ts
@@ -3,13 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent, dTableComponent, PersonalInfoModalComponent, 
          ClientStageComponent, DummyStageComponent }  from '../ngComponents/ngComponents';
 import { ListService } from '../ngServices/ngServices';
 
+const appRoutes: Routes = [
+  { path: '', redirectTo: 'client', pathMatch: 'full' },
+  { path: 'client', component: ClientStageComponent },
+  { path: 'dummy',      component: DummyStageComponent }];
+
 @NgModule({
-  imports:      [ BrowserModule, HttpModule, FormsModule ],
+  imports:      [ BrowserModule, HttpModule, FormsModule, RouterModule.forRoot(appRoutes) ],
   declarations: [ AppComponent, dTableComponent, PersonalInfoModalComponent,  
                  ClientStageComponent, DummyStageComponent ],
   providers:    [ListService],
