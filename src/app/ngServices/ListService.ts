9cd922334dfbfc6d294862ed1a0aacc191d2e366
GEORGIOS DELIANDREADIS <georgios@multiplyagency.com>
2018-01-29 15:14:57 +0000

    update servise to display http errors

--- src/app/ngServices/ListService.ts
+++ src/app/ngServices/ListService.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
+import { Observable } from "rxjs/Rx"
 import 'rxjs/add/operator/map';
 import 'rxjs/Rx';
 import { BehaviorSubject } from 'rxjs/Rx';
@@ -14,17 +15,26 @@ constructor(private _http:Http) { }
 
 getUsers(){
   return this._http.get(this._basePath + "/users")
-         .map((res:Response)=> res.json());
+         .map((res:Response)=> res.json())
+         .catch(this._errorHandler);   
 } 
 
 getPosts(){
     return this._http.get(this._basePath + "/posts")
-         .map((res:Response)=> res.json());
+         .map((res:Response)=> res.json())
+         .catch(this._errorHandler);   
 } 
 
 getCommentsByPostId(postId:any){
     return this._http.get(this._basePath + "/comments?postId=" + postId)
-          .map((res:Response)=> res.json());
+          .map((res:Response)=> res.json())
+          .catch(this._errorHandler);   
+}
+
+_errorHandler(error:Response)
+{
+  console.log(error);
+  return Observable.throw(error || "Server error");
 }
 
 } 
